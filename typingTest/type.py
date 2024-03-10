314767cd6fd4d54ba6058836101369bea14fa415
Jonquill Howlett <95897845+Here-Comes-Johnny@users.noreply.github.com>
2024-03-09 22:46:29 -0700

    add window

--- typingTest/type.py
+++ typingTest/type.py
@@ -3,9 +3,19 @@ import random
 import tkinter as tk
 
 def gui():
-    # check for tkinter version
-    # print(tk._test())
-    print(tk.Tcl().eval('info patchlevel'))
+    # print(tk.Tcl().eval('info patchlevel'))     # check for tkinter version
+    window = tk.Tk() # instance of new window
+    window.geometry("800x600") # Horizontal x Vertical
+    window.minsize(800, 600)
+    window.maxsize(1000, 800)
+
+    header = tk.Label(text = "hello world") # label for the window display purposes only
+    header.pack() # add the label to the window
+
+    button = tk.Button(window, text = "Start the Game", command= window.destroy) # atm window.destroy closes the window
+    button.pack(side = "bottom", padx=10, pady=5)
+
+    window.mainloop() # display the window and label
 
 
 def wordBank():
@@ -25,6 +35,7 @@ def wordBank():
     return word
 
 def main():
+    gui()
     filename = wordBank()
     with open(filename, 'r') as file:
         original = file.read()
