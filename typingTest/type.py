00771554c2b7d445682ab696ebcb40fb5a0e0906
Jonquill Howlett <95897845+Here-Comes-Johnny@users.noreply.github.com>
2024-03-09 23:42:51 -0700

    prints to gui

--- typingTest/type.py
+++ typingTest/type.py
@@ -2,22 +2,6 @@ from time import time
 import random
 import tkinter as tk
 
-def gui():
-    # print(tk.Tcl().eval('info patchlevel'))     # check for tkinter version
-    window = tk.Tk() # instance of new window
-    window.geometry("800x600") # Horizontal x Vertical
-    window.minsize(800, 600)
-    window.maxsize(1000, 800)
-
-    header = tk.Label(text = "hello world") # label for the window display purposes only
-    header.pack() # add the label to the window
-
-    button = tk.Button(window, text = "Start the Game", command= window.destroy) # atm window.destroy closes the window
-    button.pack(side = "bottom", padx=10, pady=5)
-
-    window.mainloop() # display the window and label
-
-
 def wordBank():
     # one = "typingTest/lovestory"
     # two = "typingTest/dontstopbelievin"
@@ -34,12 +18,37 @@ def wordBank():
     word = random.choice(choices)
     return word
 
+    
+def gui():
+    # print(tk.Tcl().eval('info patchlevel'))     # check for tkinter version
+    window = tk.Tk() # instance of new window
+    window.geometry("800x600") # Horizontal x Vertical
+    window.minsize(800, 600)
+    window.maxsize(1000, 800)
+
+    def test():
+        button.destroy()
+        filename = wordBank()
+        # print(filename)
+        with open(filename, 'r') as file:
+            original = file.read()
+        label = tk.Label(window, text=original)
+        label.pack()
+
+    header = tk.Label(text = "Typing Test") # label for the window display purposes only
+    header.pack() # add the label to the window
+
+    button = tk.Button(window, text = "Start the Game", command=test) # atm window.destroy closes the window
+    button.pack(side = "bottom", padx=10, pady=5)
+
+    window.mainloop() # display the window and label
+
 def main():
     gui()
-    filename = wordBank()
-    with open(filename, 'r') as file:
-        original = file.read()
-    print(original)
+    # filename = wordBank()
+    # with open(filename, 'r') as file:
+    #     original = file.read()
+    # print(original)
 
 if __name__ == '__main__':    
     main()
