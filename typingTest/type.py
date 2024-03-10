d49dec71e07fd2a3837c44d8b02b58622d7f8f02
Jonquill Howlett <95897845+Here-Comes-Johnny@users.noreply.github.com>
2024-03-09 22:00:04 -0700

    saving spot

--- typingTest/type.py
+++ typingTest/type.py
@@ -1,12 +1,24 @@
 from time import time
 import random
+import tkinter as tk
+
+def gui():
+    # check for tkinter version
+    # print(tk._test())
+    print(tk.Tcl().eval('info patchlevel'))
+
 
 def wordBank():
-    one = "typingTest/lovestory"
-    two = "typingTest/dontstopbelievin"
-    three = "typingTest/yesterday"
-    four = "typingTest/beforehecheats"
-    five = "typingTest/hundredyears"
+    # one = "typingTest/lovestory"
+    # two = "typingTest/dontstopbelievin"
+    # three = "typingTest/yesterday"
+    # four = "typingTest/beforehecheats"
+    # five = "typingTest/hundredyears"
+    one = "lovestory"
+    two = "dontstopbelievin"
+    three = "yesterday"
+    four = "beforehecheats"
+    five = "hundredyears"
 
     choices = [one, two, three, four, five]
     word = random.choice(choices)
