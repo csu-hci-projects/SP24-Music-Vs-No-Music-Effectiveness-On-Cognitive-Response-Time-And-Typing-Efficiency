3e33a0c09a2fe971308798641908d7416f1ec311
Jonquill Howlett <95897845+Here-Comes-Johnny@users.noreply.github.com>
2024-03-10 19:50:42 -0600

    swap word bank to class

--- typingTest/class.py
+++ typingTest/class.py
@@ -2,21 +2,21 @@ from time import time
 import random
 import tkinter as tk
 
-def wordBank():
-    # one = "typingTest/lovestory"
-    # two = "typingTest/dontstopbelievin"
-    # three = "typingTest/yesterday"
-    # four = "typingTest/beforehecheats"
-    # five = "typingTest/hundredyears"
-    one = "lovestory"
-    two = "dontstopbelievin"
-    three = "yesterday"
-    four = "beforehecheats"
-    five = "hundredyears"
+# def wordBank():
+#     # one = "typingTest/lovestory"
+#     # two = "typingTest/dontstopbelievin"
+#     # three = "typingTest/yesterday"
+#     # four = "typingTest/beforehecheats"
+#     # five = "typingTest/hundredyears"
+#     one = "lovestory"
+#     two = "dontstopbelievin"
+#     three = "yesterday"
+#     four = "beforehecheats"
+#     five = "hundredyears"
 
-    choices = [one, two, three, four, five]
-    word = random.choice(choices)
-    return word
+#     choices = [one, two, three, four, five]
+#     word = random.choice(choices)
+#     return word
 
 class gui(tk.Tk):
     def __init__(self):
@@ -32,14 +32,34 @@ class gui(tk.Tk):
         self.start = tk.Button(self, text = "Start the Game", command=self.getFile) # need to change test to function to call filename
         self.start.pack(side = "bottom", padx=10, pady=5)
 
+    def wordBank(self):
+        # one = "typingTest/lovestory"
+        # two = "typingTest/dontstopbelievin"
+        # three = "typingTest/yesterday"
+        # four = "typingTest/beforehecheats"
+        # five = "typingTest/hundredyears"
+        one = "lovestory"
+        two = "dontstopbelievin"
+        three = "yesterday"
+        four = "beforehecheats"
+        five = "hundredyears"
+
+        choices = [one, two, three, four, five]
+        word = random.choice(choices)
+        return word
+
     def getFile(self):
-        filename = wordBank()
+        filename = self.wordBank()
         # print(filename)
         with open(filename, 'r') as file:
             original = file.read()
         label = tk.Label(self, text=original)
         label.pack()
         self.start.destroy()
+
+    def userInput(self):
+        print("stuff")
+    
 
 def main():
     one = gui()
