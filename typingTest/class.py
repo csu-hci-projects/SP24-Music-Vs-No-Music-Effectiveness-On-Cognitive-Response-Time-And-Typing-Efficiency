a2c258f37611a5d89b17f75635274a2e88c8ab20
Jonquill Howlett <95897845+Here-Comes-Johnny@users.noreply.github.com>
2024-03-24 19:08:27 -0600

    fixed the font size

--- typingTest/class.py
+++ typingTest/class.py
@@ -12,7 +12,7 @@ class gui(tk.Tk):
         self.maxsize(1000, 800)
         self.title("Typing Test")
         self.config(padx=50, pady=10)
-        self.instructions = tk.Label(text = "Type the prompt. The timer start at the first keystroke. To end the timer, use the mouse to hit the text box.") 
+        self.instructions = tk.Label(font=('Arial', 14),text = "Type the prompt. The timer start at the first keystroke. To end the timer, use the mouse to hit the text box.") 
         self.instructions.pack() # add the label to the window
 
         self.file = tk.Button(self, text = "Get the Typing Test", command=self.getFile)
@@ -25,7 +25,7 @@ class gui(tk.Tk):
         # self.typingWindow.place(x=100, y = 100)
         self.typingWindow.pack(side = 'bottom')
 
-        self.typingArea = tk.Text(self.typingWindow, wrap='w', padx=5, pady=5, fg='black', bg='white')
+        self.typingArea = tk.Text(self.typingWindow, wrap='w', padx=5, pady=5, fg='black', bg='white', font='14')
         self.typingArea.pack()
 
         self.done = tk.Button(self, text='Done', command=self.checkResults)
@@ -58,7 +58,7 @@ class gui(tk.Tk):
             self.song = original
             self.inputWords = original.split()
             self.totalWords = len(self.inputWords)
-        label = tk.Label(self, text=original)
+        label = tk.Label(self, text=original, font=('Arial', 14))
         label.pack()
         self.file.destroy()
         self.userInput()
@@ -74,15 +74,16 @@ class gui(tk.Tk):
         self.inputFromUser = temp.split()
         self.accuracy = (sum(1 for x,y in zip(self.inputWords, self.inputFromUser) if x ==y) / self.totalWords) * 100
         if self.accuracy < 100:
-            accuracyLabel = tk.Label(self, text = f'{self.accuracy:.3}% accuracy')
+            accuracyLabel = tk.Label(self, text = f'{self.accuracy:.3}% accuracy', font=('Arial', 20))
         else:
-            accuracyLabel = tk.Label(self, text = f'{self.accuracy}% accuracy')
+            accuracyLabel = tk.Label(self, text = f'{self.accuracy}% accuracy', font=('Arial', 20))
         accuracyLabel.pack()
+        self.typingArea.destroy()
 
     def checkResults(self):
         self.endTime = time.time()
         self.done.destroy()
-        timeLabel = tk.Label(self, text = f'Attempt took {time.strftime("%H:%M:%S", time.gmtime(self.endTime - self.startTime))}')
+        timeLabel = tk.Label(self, text = f'Attempt took {time.strftime("%H:%M:%S", time.gmtime(self.endTime - self.startTime))}', font=('Arial', 20))
         timeLabel.pack()
         self.userAccuracy()
 
